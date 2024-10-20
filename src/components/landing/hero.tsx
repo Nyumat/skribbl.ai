f8e811d343d9fe8e84b77e219e6616b2f773c1da
Thomas <nyumat18@gmail.com>
2024-10-19 21:35:17 -0700

    hero / cta

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -1,38 +1,94 @@
-import Image from "next/image"
-import Link from "next/link"
-import { buttonVariants } from "~/components/ui/button"
-import { cn } from "~/lib/utils"
+"use client";
 
-export default function HeroHeader() {
+import { motion } from "framer-motion";
+import {
+    ArrowRight,
+    Github,
+    MoveRight,
+} from "lucide-react";
+import { type Session } from "next-auth";
+import { useRouter } from "next/navigation";
+import { useEffect, useMemo, useState } from "react";
+import { Button } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
+
+export function Hero({ session }: { session: Session | null }) {
+    const router = useRouter();
+    const [titleNumber, setTitleNumber] = useState(0);
+    const titles = useMemo(
+        () => ["Friends", "Teams", "Collaboration"],
+        []
+    );
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            if (titleNumber === titles.length - 1) {
+                setTitleNumber(0);
+            } else {
+                setTitleNumber(titleNumber + 1);
+            }
+        }, 2000);
+        return () => clearTimeout(timeoutId);
+    }, [titleNumber, titles]);
+
     return (
-        <section className="container flex flex-col gap-4 pb-12 pt-4 text-center lg:items-center lg:gap-8 lg:py-20">
-            <div className="flex flex-1 flex-col items-center gap-4 text-center lg:gap-8">
-                <div className="space-y-4">
-                    <h1 className="text-4xl font-bold lg:text-6xl">
-                        Draw Based on a Prompt with Muse
-                    </h1>
-                    <h2 className="text-lg font-light text-muted-foreground lg:text-3xl">
-                        We give you a prompt, you draw it. AI Judges it. Now
-                    </h2>
+        <div className="w-full">
+            <div className="container mx-auto">
+                <div className="flex gap-8 py-12 items-center justify-center flex-col">
+                    <div>
+                        <Button variant="secondary" size="sm" className="gap-4" onClick={() => window.open("https://devpost.com/software/skribbl.ai")}>
+                            skribbl.ai on devpost! <MoveRight className="w-4 h-4" />
+                        </Button>
+                    </div>
+                    <div className="flex gap-4 flex-col">
+                        <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
+                            <span className="text-spektr-cyan-50">The Gameified Whiteboard for</span>
+                            <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
+                                &nbsp;
+                                {titles.map((title, index) => (
+                                    <motion.span
+                                        key={index}
+                                        className="absolute font-semibold"
+                                        initial={{ opacity: 0, y: "-100" }}
+                                        transition={{ type: "spring", stiffness: 50 }}
+                                        animate={
+                                            titleNumber === index
+                                                ? {
+                                                    y: 0,
+                                                    opacity: 1,
+                                                }
+                                                : {
+                                                    y: titleNumber > index ? -150 : 150,
+                                                    opacity: 0,
+                                                }
+                                        }
+                                    >
+                                        {title}
+                                    </motion.span>
+                                ))}
+                            </span>
+                        </h1>
+
+                        <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
+                            skribbl.ai is a gameified whiteboard platform that allows users to play games like Pictionary, Charades, and more with their friends and teams.
+                        </p>
+                    </div>
+                    <div className="flex flex-row gap-3">
+                        <Button size="lg" className={cn("gap-4 ring-1 ring-blue-600 hover:bg-blue-600")} variant="outline" onClick={() => {
+                            if (session) {
+                                router.push("/platform/choose");
+                            } else {
+                                router.push("/auth/signup");
+                            }
+                        }}>
+                            Get started <ArrowRight className="w-6 h-6" />
+                        </Button>
+                        <Button size="lg" className="gap-2" variant="secondary">
+                            GitHub <Github className="w-6 h-6" />
+                        </Button>
+                    </div>
                 </div>
-                <Link
-                    href="https://github.com/nyumat/muse"
-                    target="_blank"
-                    className={`w-[10rem] ${cn(buttonVariants({ size: "lg" }))}`}
-                >
-                    Get started
-                </Link>
             </div>
-
-            <div className="flex flex-1 justify-center lg:justify-end">
-                <Image
-                    src="/images/hero.svg"
-                    width={500}
-                    height={500}
-                    alt="Header image"
-                />
-            </div>
-
-        </section>
-    )
-}
+        </div>
+    );
+};
