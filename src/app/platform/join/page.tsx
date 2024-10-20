da1f5dbf802c2a3d88ab71cadb6ba609e0871a41
Thomas <nyumat18@gmail.com>
2024-10-19 23:15:21 -0700

    join room page

--- src/app/platform/join/page.tsx
+++ src/app/platform/join/page.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
-
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
+import { SkribblLogo } from '~/components/logo'
+import { Button } from '~/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader } from '~/components/ui/card'
+import { Input } from '~/components/ui/input'
+import { Label } from '~/components/ui/label'
 export default function JoinRoomPage() {
-  return (
-    <div>Join a room</div>
-  )
+    return (
+        <>
+            <div className="min-h-screen flex flex-col items-center justify-center">
+                <div className="absolute top-4 left-4 m-12">
+                    <Link href="/platform/choose">
+                        <ArrowLeft
+                            className="cursor-pointer text-blue-600"
+                            size={20}
+                        />
+                        <span className="text-sm">Back</span>
+                    </Link>
+                </div>
+                <SkribblLogo className="mb-8" />
+                <Card className="w-full max-w-md">
+                    <CardHeader>
+                        <h1 className="text-3xl font-bold">Join a Room</h1>
+                        <p className="text-gray-400">Ask your room host for the room code.</p>
+                    </CardHeader>
+                    <CardContent>
+                        <Label>Room Code</Label>
+                        <Input placeholder="Enter the room code" />
+                    </CardContent>
+                    <CardFooter>
+                        {/* TODO: Make this route to said room */}
+                        <Button className="w-full">Join Room</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+        </>
+    )
 }
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,15 @@
 import { PencilRuler } from 'lucide-react'
 import Link from 'next/link'
+import { cn } from '~/lib/utils'
 
-export function SkribblLogo() {
+export function SkribblLogo({ className }: { className?: string }) {
     return (
         <>
-            <div className='inline-flex items-center'>
-            <PencilRuler className="text-blue-600 translate-y-[0.8px]" />
-            <Link href="/" passHref className="ml-3 text-xl">
-                skribbl.ai
-            </Link>
+            <div className={cn('inline-flex items-center', className)}>
+                <PencilRuler className="text-blue-600 translate-y-[0.8px]" />
+                <Link href="/" passHref className="ml-3 text-xl">
+                    skribbl.ai
+                </Link>
             </div>
         </>
     )
