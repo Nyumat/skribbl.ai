dfd4f7eaca114a03f5c4be369ffdf5ccc0967e23
Thomas <nyumat18@gmail.com>
2024-10-20 09:11:49 -0700

    what's going on

--- src/env.js
+++ src/env.js
@@ -40,11 +40,11 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
     DATABASE_URL:
       process.env.NODE_ENV == "development"
         ? process.env.DEV_DATABASE_URL
         : process.env.PROD_DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
