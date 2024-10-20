923b8f0861bac738fb299466dbebc5cd9eb02582
Thomas <nyumat18@gmail.com>
2024-10-19 22:00:07 -0700

    seo bufffffffs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,15 @@ import { type Metadata } from "next";
 import { ThemeProvider } from "~/components/theme-provider";
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://drawing-is-for-kids-they-say.vercel.app/"),
     title: "Skribbl.ai - The Gameified Whiteboard for Friends, Teams, and Collaboration",
     description: "skribbl.ai is a gameified whiteboard for friends, teams, and collaboration. Play games, draw, and have fun with your friends!",
     icons: [{ rel: "icon", url: "/favicon.ico" }],
+    openGraph: {
+        title: "Skribbl.ai - The Gameified Whiteboard for Friends, Teams, and Collaboration",
+        description: "skribbl.ai is a gameified whiteboard for friends, teams, and collaboration. Play games, draw, and have fun with your friends!",
+        images: [{ url: "/og.png" }],
+    },
 };
 
 export default function RootLayout({
