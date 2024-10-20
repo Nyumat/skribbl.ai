0302ab13bbf8b67ffa437234233de4fb7e216fb9
Thomas <nyumat18@gmail.com>
2024-10-20 04:33:02 -0700

    freeplay draw page

--- src/app/platform/freeplay/page.tsx
+++ src/app/platform/freeplay/page.tsx
@@ -1,1 +1,21 @@
-rf
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import { Tldraw } from '@tldraw/tldraw';
+import '@tldraw/tldraw/tldraw.css';
+
+export default function WhiteboardPage({
+    searchParams,
+}: {
+    params: { slug: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+    }) {
+    const persistKey = searchParams.slug as string
+    return (
+        <div className="flex h-screen bg-black text-white">
+            <div className="flex-1 bg-gray-900 border border-gray-800">
+                <Tldraw />
+            </div>
+        </div>
+    )
+}
