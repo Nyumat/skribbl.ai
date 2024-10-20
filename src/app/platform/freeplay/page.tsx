d9471c2c8d057a9af9241ab7d500d6dad0dfc725
Thomas <nyumat18@gmail.com>
2024-10-20 06:43:12 -0700

    add dark mode support for editor

--- src/app/platform/freeplay/page.tsx
+++ src/app/platform/freeplay/page.tsx
@@ -1,20 +1,32 @@
 /* eslint-disable react/no-unescaped-entities */
 "use client";
 
-import { Tldraw } from '@tldraw/tldraw';
+import { setUserPreferences, Tldraw } from '@tldraw/tldraw';
 import '@tldraw/tldraw/tldraw.css';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function WhiteboardPage({
     searchParams,
 }: {
     params: { slug: string }
     searchParams: { [key: string]: string | string[] | undefined }
-    }) {
+}) {
     const persistKey = searchParams.slug as string
+    const { data } = useSession();
+    const userId = data?.user?.id;
+    useEffect(() => {
+        if (userId) {
+            setUserPreferences({
+                colorScheme: "system",
+                id: userId
+            });
+        }
+    }, [userId]);
     return (
         <div className="flex h-screen bg-black text-white">
             <div className="flex-1 bg-gray-900 border border-gray-800">
-                <Tldraw />
+                <Tldraw persistenceKey={userId} />
             </div>
         </div>
     )
