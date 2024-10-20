bf5584804a1b3100701341f99ce2ac03ecbe2a86
Thomas <nyumat18@gmail.com>
2024-10-19 21:34:46 -0700

    stub routing until we finish these pages

---
+++ src/app/platform/create/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateRoomPage() {
+  return (
+    <div>Create a Room</div>
+  )
+}
---
+++ src/app/platform/join/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function JoinRoomPage() {
+  return (
+    <div>Join a room</div>
+  )
+}
